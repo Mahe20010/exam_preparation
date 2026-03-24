76683f3b79c0d65a3c736d54977f59a732e39c8f
Mahe20010 <97661016+Mahe20010@users.noreply.github.com>
2026-03-24 14:22:17 +0530

    Comment out PdfTextExtractor class and methods

--- src/main/java/com/example/demo/helper/PdfTextExtractor.java
+++ src/main/java/com/example/demo/helper/PdfTextExtractor.java
@@ -1,19 +1,19 @@
-package com.example.demo.helper;
+// package com.example.demo.helper;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
+// import org.apache.pdfbox.pdmodel.PDDocument;
+// import org.apache.pdfbox.text.PDFTextStripper;
 
-import java.nio.file.Path;
+// import java.nio.file.Path;
 
-public class PdfTextExtractor {
+// public class PdfTextExtractor {
 
-    public static String extract(Path path) {
-        try (PDDocument doc = PDDocument.load(path.toFile())) {
-            PDFTextStripper stripper = new PDFTextStripper();
-            return stripper.getText(doc);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "";
-        }
-    }
-}
+//     public static String extract(Path path) {
+//         try (PDDocument doc = PDDocument.load(path.toFile())) {
+//             PDFTextStripper stripper = new PDFTextStripper();
+//             return stripper.getText(doc);
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//             return "";
+//         }
+//     }
+// }
