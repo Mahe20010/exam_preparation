1daf0a740d05963395f5436dae5e2053ae15bc46
Mahe20010 <97661016+Mahe20010@users.noreply.github.com>
2026-03-24 14:21:27 +0530

    Comment out OCRExtractor class and its methods
    Commented out the entire OCRExtractor class implementation.

--- src/main/java/com/example/demo/helper/OCRExtractor.java
+++ src/main/java/com/example/demo/helper/OCRExtractor.java
@@ -1,38 +1,38 @@
-package com.example.demo.helper;
-import net.sourceforge.tess4j.*;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.rendering.PDFRenderer;
+// package com.example.demo.helper;
+// import net.sourceforge.tess4j.*;
+// import org.apache.pdfbox.pdmodel.PDDocument;
+// import org.apache.pdfbox.rendering.PDFRenderer;
 
-import java.awt.image.BufferedImage;
-import java.nio.file.Path;
+// import java.awt.image.BufferedImage;
+// import java.nio.file.Path;
 
-public class OCRExtractor {
+// public class OCRExtractor {
 
-    public static String extractText(Path path) {
+//     public static String extractText(Path path) {
 
-        StringBuilder text = new StringBuilder();
+//         StringBuilder text = new StringBuilder();
 
-        try (PDDocument doc = PDDocument.load(path.toFile())) {
+//         try (PDDocument doc = PDDocument.load(path.toFile())) {
 
-            PDFRenderer renderer = new PDFRenderer(doc);
-            ITesseract tesseract = new Tesseract();
+//             PDFRenderer renderer = new PDFRenderer(doc);
+//             ITesseract tesseract = new Tesseract();
 
-            // 👉 SET YOUR TESSDATA PATH
-            tesseract.setDatapath("C:/Program Files/Tesseract-OCR/tessdata");
+//             // 👉 SET YOUR TESSDATA PATH
+//             tesseract.setDatapath("C:/Program Files/Tesseract-OCR/tessdata");
 
-            for (int i = 0; i < doc.getNumberOfPages(); i++) {
+//             for (int i = 0; i < doc.getNumberOfPages(); i++) {
 
-                BufferedImage image = renderer.renderImageWithDPI(i, 300);
+//                 BufferedImage image = renderer.renderImageWithDPI(i, 300);
 
-                String pageText = tesseract.doOCR(image);
+//                 String pageText = tesseract.doOCR(image);
 
-                text.append(pageText).append("\n");
-            }
+//                 text.append(pageText).append("\n");
+//             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
 
-        return text.toString();
-    }
-}
+//         return text.toString();
+//     }
+// }
