e537e6192b17fa2bfbb871bf30f81e5166e885ed
Mahe20010 <97661016+Mahe20010@users.noreply.github.com>
2026-03-24 14:22:37 +0530

    Comment out PdfToImageConverter class code

--- src/main/java/com/example/demo/helper/PdfToImageConverter.java
+++ src/main/java/com/example/demo/helper/PdfToImageConverter.java
@@ -1,32 +1,32 @@
-package com.example.demo.helper;
+// package com.example.demo.helper;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.rendering.PDFRenderer;
+// import org.apache.pdfbox.pdmodel.PDDocument;
+// import org.apache.pdfbox.rendering.PDFRenderer;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+// import java.awt.image.BufferedImage;
+// import java.io.File;
+// import java.util.ArrayList;
+// import java.util.List;
 
-public class PdfToImageConverter {
+// public class PdfToImageConverter {
 
-    public static List<BufferedImage> convert(File file) {
+//     public static List<BufferedImage> convert(File file) {
 
-        List<BufferedImage> images = new ArrayList<>();
+//         List<BufferedImage> images = new ArrayList<>();
 
-        try (PDDocument doc = PDDocument.load(file)) {
+//         try (PDDocument doc = PDDocument.load(file)) {
 
-            PDFRenderer renderer = new PDFRenderer(doc);
+//             PDFRenderer renderer = new PDFRenderer(doc);
 
-            for (int i = 0; i < doc.getNumberOfPages(); i++) {
-                BufferedImage image = renderer.renderImageWithDPI(i, 300);
-                images.add(image);
-            }
+//             for (int i = 0; i < doc.getNumberOfPages(); i++) {
+//                 BufferedImage image = renderer.renderImageWithDPI(i, 300);
+//                 images.add(image);
+//             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
 
-        return images;
-    }
-}
+//         return images;
+//     }
+// }
